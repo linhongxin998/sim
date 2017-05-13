669d7a20a51d73b7477a40a7ab0e9aa4243ca120
qiqiangvae <779676455@qq.com>
2017-05-13 19:15:22 +0800

    eclipse提交测试

--- src/main/java/com/gyq/sim/dao/ResearchProjectDao.java
+++ src/main/java/com/gyq/sim/dao/ResearchProjectDao.java
@@ -2,12 +2,15 @@ package com.gyq.sim.dao;
 
 import java.util.List;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import com.gyq.sim.entity.ResearchProject;
 
 public interface ResearchProjectDao {
 
 	List<ResearchProject> getResearchProjectList(ResearchProject project);
 
+	@Transactional
 	void addResearchProject(ResearchProject addproject);
 
 	void editResearchProject(ResearchProject project);
